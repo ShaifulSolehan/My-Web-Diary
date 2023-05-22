f61f19bd81daa053d9ebd6e91b9610681b772862
ShaifulSolehan <shaifulsolehan@gmail.com>
2023-05-22 23:23:25 +0800

    add backend fetch data from entry form and save it into list

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import datetime
 
 app = Flask(__name__,template_folder="templates")
 
-@app.route("/")
+entries = []
+@app.route("/",methods = ["GET","POST"])
 def main():
-    return render_template("home.html")
+    if request.method == "POST":
+        diary_content = request.form.get("content")
+        formatteddate = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((diary_content,formatteddate))
+
+    entries_with_date=[
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+        
+    return render_template("home.html",entries = entries_with_date)
 
